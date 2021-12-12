4cd0c4709a5e0529fd80edebb9b254c15e6d0d66
Marc Teixeira <MTeixeira@my.gcu.edu>
2021-12-12 11:47:20 -0800

    Updated DDL scripts to new database tables.

--- DDLScript.sql
+++ DDLScript.sql
@@ -1,12 +1,17 @@
-CREATE SCHEMA IF NOT EXISTS `MinesweeperApp` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `MinesweeperApp` ;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Table `MinesweeperApp`.`users`
+-- Schema cst-350
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `MinesweeperApp`.`users` ;
+CREATE SCHEMA IF NOT EXISTS `cst-350` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `cst-350` ;
 
-CREATE TABLE IF NOT EXISTS `MinesweeperApp`.`users` (
+-- -----------------------------------------------------
+-- Table `cst-350`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cst-350`.`users` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `FIRSTNAME` VARCHAR(40) NOT NULL,
   `LASTNAME` VARCHAR(40) NOT NULL,
@@ -23,3 +28,50 @@ CREATE TABLE IF NOT EXISTS `MinesweeperApp`.`users` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `cst-350`.`boards`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cst-350`.`boards` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `USER_ID` INT NOT NULL,
+  `SIZE` INT NOT NULL,
+  `DIFFICULTY` INT NOT NULL,
+  `NUMBEROFMINES` INT NOT NULL,
+  `TIMESTARTED` DATETIME NOT NULL,
+  `TIMEPLAYED` TIME NOT NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `FK_BOARDS_TO_USERS_idx` (`USER_ID` ASC) VISIBLE,
+  CONSTRAINT `FK_BOARDS_TO_USERS`
+    FOREIGN KEY (`USER_ID`)
+    REFERENCES `cst-350`.`users` (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `cst-350`.`cells`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cst-350`.`cells` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `CELL_ID` INT NOT NULL,
+  `VISITED` BIT(1) NOT NULL,
+  `MINE` BIT(1) NOT NULL,
+  `FLAGGED` BIT(1) NOT NULL,
+  `LIVENEIGHBORS` INT NOT NULL,
+  `BOARD_ID` INT NOT NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `FK_CELLS_TO_BOARDS_idx` (`BOARD_ID` ASC) VISIBLE,
+  CONSTRAINT `FK_CELLS_TO_BOARDS`
+    FOREIGN KEY (`BOARD_ID`)
+    REFERENCES `cst-350`.`boards` (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
